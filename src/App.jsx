6e61e97b474c459654787e50ad761fb07f68d0dd
Fonate-Michael <developerfonate@gmail.com>
2025-08-05 14:28:42 +0100

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -24,75 +24,79 @@ function App() {
   };
 
   return (
-    <div className="login-container">
+    <div className="app-container">
       {/* Header avec logo et barre de recherche */}
       <header className="flearnaxy-header">
-        <div className="logo-container">
-          <img 
-            src={flearnaxylogo}
-            alt="flearnaxy Logo" 
-            className="logo"
-          />
-        </div>
-        <div className="flearnaxy-contain">
-          <img src={flearnaxy} alt="" className="flearimg"/>
-        </div>
-        <div className="search-bar">
-          <input type="text" placeholder="Search flearnaxy" />
-        </div>
-        <div className="signin1">
-          <fieldset>
-            <p>signin</p>
-          </fieldset>
-        </div>
-        <div className="signup1">
-          <fieldset>
-            <p>signup</p>
-          </fieldset>
+        <div className="header-content">
+          <div className="logo-container">
+            <img
+              src={flearnaxylogo}
+              alt="flearnaxy Logo"
+              className="logo"
+            />
+          </div>
+          <div className="flearnaxy-contain">
+            Flearnaxy
+          </div>
+          <div className="search-bar">
+            <input type="text" placeholder="Search flearnaxy" />
+          </div>
+          <div className="auth-buttons">
+            <div className="signin1">
+              <fieldset>
+                <p>signin</p>
+              </fieldset>
+            </div>
+            <div className="signup1">
+              <fieldset>
+                <p>signup</p>
+              </fieldset>
+            </div>
+          </div>
         </div>
       </header>
 
       <main className="signin-contain">
-        <img src={flearnaxysignin} alt="" />  
+        <div className="logo-label">Flearnaxy Sign in</div>
 
         <form className="signin-form" onSubmit={handleSubmit}>
           <div className="form-section">
-            <input 
-              type="email" 
+            <input
+              type="email"
               name="email"
-              value={formData.email} 
+              value={formData.email}
               onChange={handleChange}
-              placeholder='Email' 
+              placeholder='Email'
             />
-            <input 
-              type="password" 
+            <input
+              type="password"
               name="password"
-              value={formData.password} 
+              value={formData.password}
               onChange={handleChange}
-              placeholder='Mot de passe'
+              placeholder='Password'
             />
           </div>
           <div className="forgotpwd">
-            <a href="#" className="forgot">Fogot password ?</a>
+            <a href="#" className="forgot">Forgot password ?</a>
           </div>
 
-          <button type='submit' className="signin-button">Sign in</button>
+          <button type='submit' className="signin-button">Sign In</button>
         </form>
 
         <div className="social-login">
           <div className="social-text">or continue with</div>
           <div className="social-buttons">
             <button className="social-button apple">
-              <img 
-                src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg" 
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg"
                 alt="Apple Logo"
                 className="social-icon"
               />
               Apple
             </button>
             <button className="social-button google">
-              <img 
-                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" 
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
                 alt="Google Logo"
                 className="social-icon"
               />
